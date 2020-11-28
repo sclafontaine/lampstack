b7f9d7c71497da9c8e45adf5ff728c1db826a226
sclafontaine <seanlafontaine97@gmail.com>
2020-11-27 18:49:06 -0600

    This is the third push to Github Repository

--- application/config/database.php
+++ application/config/database.php
@@ -75,7 +75,7 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => '172.17.0.5',
+	'hostname' => '172.17.0.1',
 	'username' => 'ci_user',
 	'password' => 'cipassword',
 	'database' => 'cidatabase',
